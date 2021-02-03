0749372b688f8c10beda5c18efd80d594bdcffa0
hongyeongjune <hongyeongjune@naver.com>
2021-02-03 16:51:59 +0900

    Multi Module Init

---
+++ src/main/java/com/wap/weareplayer/WeareplayerApplication.java
@@ -0,0 +1,13 @@
+package com.wap.weareplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeareplayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeareplayerApplication.class, args);
+    }
+
+}
